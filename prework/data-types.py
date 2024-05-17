eb1788761af96fa7c496eacefdd98758c00bff09
Billy <bjbaneydev@gmail.com>
2024-05-16 22:55:34 -0500

    pushing beginning of prework to github, includes data types section

---
+++ prework/data-types.py
@@ -0,0 +1,16 @@
+# literals - directly represent an object in source code
+'Hello, world!' # str literal
+3.141592 # float literal
+True # boolean literal
+{'a' : 1, 'b' : 2} # dict literal
+[1, 2, 3] # list literal
+(4, 5, 6) # tuple literal
+{7, 8, 9} # set literal
+
+# not all objets have literal forms. In those cases, we use the type constructor to create objects of the type
+range(10) # range of numbers: 0 - 9
+range(1, 11) # range of numbers 1 - 10
+set() # empty set
+frozenset([1, 2]) # Frozen set of values: 1 and 2
+
+# Numeric Values
